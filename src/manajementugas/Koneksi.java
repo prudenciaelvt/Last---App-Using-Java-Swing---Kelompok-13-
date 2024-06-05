ef40bedf8e480b2a324db4806b8439c9749754fe
DanielPrasetio28 <danielprasetiobudiman@gmail.com>
2024-06-05 12:59:54 +0700

    new dir

---
+++ src/manajementugas/Koneksi.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+package manajementugas;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Koneksi {
+    private static java.sql.Connection koneksi;
+    
+    public static java.sql.Connection getKoneksi(){
+        if (koneksi == null){
+            try{
+                String url = "jdbc:mysql://localhost:3306/manajemen_tugas";
+                String user = "root";
+                String password = "";
+                DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+                koneksi = DriverManager.getConnection(url, user, password);
+                System.out.println("Connection Successfully");
+            }catch (Exception e){
+                System.out.println("Error");
+            }
+        }
+        return koneksi;
+    }
+    
+    public static void main(String args[]){
+        getKoneksi();
+    }
+}
